995a92d6b3a216d2fb0501d7bb33243acf25e982
mahmoud2000yas <117920378+mahmoud2000yas@users.noreply.github.com>
2023-09-18 14:00:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains3 = document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river = document.getElementById('river');
+let boat6 = document.getElementById('boat');
+let nouvil = document.querySelector('.nouvil');
+window.onscroll =function(){
+    let value = scrollY;
+stars.style.left = value + 'px';
+moon.style.top = value  * 4 + 'px';
+mountains3.style.top = value * 2 + 'px';
+mountains4.style.top = value * 1.5 + 'px';
+river.style.top = value + 'px';
+boat6.style.top = value + 'px';
+boat6.style.top = value + 'px';
+boat6.style.left = value * 3 + 'px';
+nouvil.style.fontSize = value + 'px';
+if(scrollY >= 67){
+    nouvil.style.fontSize = 67 + 'px';
+    nouvil.style.position ='fixed';
+if(scrollY >= 478){
+    nouvil.style.display = 'none';
+}else{
+    nouvil.style.display = 'block';
+}
+if(scrollY >= 127){
+    document.querySelector('.main').style.background = 'linear-gradient(#376281 , #10001f)'
+
+
+
+
+}else{
+        document.querySelector('.main').style.background = 'linear-gradient(#200016 , #10001f)'
+
+}
+}
+}
